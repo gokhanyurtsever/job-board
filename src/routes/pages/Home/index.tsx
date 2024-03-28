d5c033b8460dffbfd10e95b69feb97d8d22d5930
gokhanyurtsever <73485076+gokhanyurtsever@users.noreply.github.com>
2024-03-29 00:25:10 +0300

    services and welcomet text component added

--- src/routes/pages/Home/index.tsx
+++ src/routes/pages/Home/index.tsx
@@ -1,10 +1,20 @@
 import Hero from "../../../components/Hero";
+import Services from "../../../components/Services";
+import WelcomeText from "../../../components/WelcomeText";
+
 
 
 const Home = () => {
   return (
     <div className="w-full">
       <Hero />
+      <Services />
+      <div className="w-full mt-12 mb-16">
+        <WelcomeText />
+        <div className="w-full flex md:flex-row flex-col items-start relative md:px-16 px-5 gap-9">
+          
+        </div>
+      </div>
     </div>
   );
 }
